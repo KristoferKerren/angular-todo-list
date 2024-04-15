6fab6b2db2d24a09e3341ea5b6b52bb1d695153c
Kristofer Kerren <kristofer.kerren@crowdcollective.com>
2024-04-15 13:49:11 +0200

    Add dynamic prop to component

--- src/components/header.component.ts
+++ src/components/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 
 @Component({
@@ -6,5 +6,11 @@ import { Component } from "@angular/core";
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Input() title: string;
 
+    count: number;
+
+    ngOnInit() {
+        console.log(this.title);
+    }
 }
